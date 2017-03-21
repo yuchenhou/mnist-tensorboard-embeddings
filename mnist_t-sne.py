3da91ce41eec81c286bf50cfef83f242bb6be85e
Alan Hou <houych@gmail.com>
2017-03-21 00:23:55 -0700

    fix path

--- mnist_t-sne.py
+++ mnist_t-sne.py
@@ -1,23 +1,3 @@
-# Copyright 2016 Norman Heckscher. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""MNIST dimensionality reduction with TensorFlow and TensorBoard.
-
-This demonstrates the functionality of the TensorBoard Embedding Visualization dashboard using MNIST.
-
-https://www.tensorflow.org/versions/r0.12/how_tos/embedding_viz/index.html#tensorboard-embedding-visualization
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -94,9 +74,9 @@ if __name__ == '__main__':
                         help='If true, uses fake data for unit testing.')
     parser.add_argument('--max_steps', type=int, default=10000,
                         help='Number of steps to run trainer.')
-    parser.add_argument('--data_dir', type=str, default='/Users/norman/Documents/workspace/mnist-tensorboard-embeddings/mnist_data',
+    parser.add_argument('--data_dir', type=str, default='/home/yuchen/PycharmProjects/mnist-tensorboard-embeddings/mnist_data',
                         help='Directory for storing input data')
-    parser.add_argument('--log_dir', type=str, default='/Users/norman/Documents/workspace/mnist-tensorboard-embeddings/logs',
+    parser.add_argument('--log_dir', type=str, default='/home/yuchen/PycharmProjects/mnist-tensorboard-embeddings/logs',
                         help='Summaries log directory')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
